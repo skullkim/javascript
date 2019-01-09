298f3b8e88af265c1aa886529dfc6fe9ba32c2b6
skullkim <space1878@naver.com>
2019-01-09 22:07:35 +0900

    DOM1

---
+++ function/DOM_function.js
@@ -0,0 +1,6 @@
+// const title = document.getElementById("title");
+const title = document.querySelector("#title");
+title.style.color = "red";
+console.log(title);
+console.log(document);
+document.title = "I own you now";

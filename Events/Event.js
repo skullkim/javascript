01115b42164cbe06974ca918aafbe702f8bfe263
skullkim <space1878@naver.com>
2019-01-09 22:44:52 +0900

    Event + if

--- Events/Event.js
+++ Events/Event.js
@@ -1,7 +1,14 @@
-const title = document.querySelector("#title");
+//Events
+// const title = document.querySelector("#title");
 
-function handleResize(evnet){
-    title.style.color = "blue";
-}
+// function handleResize(evnet){
+//     title.style.color = "blue";
+// }
 
-title.addEventListener("click", handleResize);//Event가 진행될때 까지(resize 될떄까지) 기다렸다가 handleResize를 실행함
+// title.addEventListener("click", handleResize);//Event가 진행될때 까지(resize 될떄까지) 기다렸다가 handleResize를 실행함
+
+//if else + evnets
+const age = prompt("How old are you");
+if(age >= 18 && age <= 21) console.log("You can drink but you should not");
+else if(age > 21) console.log("go ahead");
+else console.log("you are too young");

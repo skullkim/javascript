99790357ed64e5b9155cade0bf89af66b93987f2
skullkim <space1878@naver.com>
2019-01-09 16:58:49 +0900

    const-let-var

---
+++ variable/const-let-var.js
@@ -0,0 +1,5 @@
+const a = 221;//constant variable, cannot change value
+let b = a -5;//let allows change value
+var c = 3;//similar with let, but not recommended
+a = 4;//Error:Assignment to consttant variable
+console.log(b.a);

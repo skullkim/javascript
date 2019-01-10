595356e2c6e6151be984d76ce8667101d2dbd9c9
skullkim <space1878@naver.com>
2019-01-11 05:20:41 +0900

    clock

---
+++ simple_practice/clock.js
@@ -0,0 +1,33 @@
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle =clockContainer.querySelector("h1");
+
+function returnSeconds(date){
+    const seconds = date.getSeconds();
+    return seconds < 10 ? `0${seconds}` : seconds;
+}
+
+function returnMinutes(date){
+    const minutes = date.getMinutes();
+    return minutes < 10 ? `0${minutes}` : minutes;
+}
+
+function returnHours(date){
+    const hours = date.getHours();
+    return hours < 10 ? `0${hours}` : hours;
+}
+
+
+function getTime(){
+    const date = new Date();
+    // const minutes = date.getMinutes();
+    // const hours = date.getHours();
+    // const seconds = date.getSeconds();
+    clockTitle.innerText = `${returnHours(date)}:${returnMinutes(date)}:${returnSeconds(date)}`;
+}
+
+function init(){
+    getTime();
+    setInterval(getTime, 1000);
+}
+
+init();
